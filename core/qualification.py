7b02521251b62ea0556c0c3e2528228ae9b4e962
Wetzel-Eric <47600055+Wetzel-Eric@users.noreply.github.com>
2026-02-20 22:28:24 +0100

    Update qualification.py

--- core/qualification.py
+++ core/qualification.py
@@ -3,7 +3,7 @@ from typing import Dict, List, Tuple
 QUESTIONS: List[Tuple[str, str]] = [
     ("pain", "Quelle problématique prioritaire ce recrutement doit-il permettre de résoudre ?"),
     ("tempo", "Quel est l’horizon temporel ?"),
-    ("techno", "Quelles technologies doivent être maîtrisées pour ce poste ?")
+    ("techno", "Quelles technologies doivent être maîtrisées pour ce poste ?"),
     ("success", "À quoi ressemble le succès dans 6 mois ?"),
 ]
 
